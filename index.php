52c22cc5c765410e55dcc01cddc0d3d1757546eb
franciscalaureano <mariakikalaureano1@gmail.com>
2021-05-17 15:27:08 +0100

    alterei cenas index, mapa js e css styles

--- index.php
+++ index.php
@@ -5,13 +5,18 @@
     <title>Balada Tipográfica</title>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/geral.css">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+
+    <!-- Links necessários para o mapa -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+
+    <!--<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
           crossorigin=""/>
-    <!-- Make sure you put this AFTER Leaflet's CSS -->
+   Make sure you put this AFTER Leaflet's CSS
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
             integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-            crossorigin=""></script>
+            crossorigin=""></script> -->
 
 </head>
 <?php
@@ -34,11 +39,13 @@ $fileContentsnovo = file_get_contents('words.txt');
 <div id="mapid"></div>
 
 <script src="comp_tipo.js"></script>
+
 <script>
     var novaspalavras=<?php echo $fileContentsnovo ?>;
 
     compPalavras(novaspalavras);
 </script>
+
 <script src="mapa.js"></script>
 </body>
 </html>
--- mapa.js
+++ mapa.js
@@ -1,4 +1,7 @@
 var mymap = L.map('mapid').setView([40.2056400, -8.4195500], 13);
+L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=WdcTGuG5ljgDFUUE3uo3',{
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(mymap);
 
 /*L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
