f79d3ed9e367667425fafa4ae5e8e80ac3348907
franciscalaureano <mariakikalaureano1@gmail.com>
2021-05-17 14:04:38 +0100

    alterei cenas index

--- mapa.js
+++ mapa.js
@@ -1,10 +1,10 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([40.2056400, -8.4195500], 13);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+/*L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+}).addTo(mymap);*/
