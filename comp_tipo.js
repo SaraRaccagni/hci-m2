fb834aa4f7b6ee4e75115334fe2b3be3555350ed
franciscalaureano <mariakikalaureano1@gmail.com>
2021-05-22 12:43:44 +0100

    novo mapa palavras ok

--- comp_tipo.js
+++ comp_tipo.js
@@ -1,7 +1,130 @@
 
 function compPalavras(palavras2) {
-    var max=0;
-    for (var prop in palavras2) {
+
+//-----------------------------------------------------------------------
+//----------------------- COMPOSIÇÃO TIPOGRÁFICA -------------------------
+//------------------------------------------------------------------------
+
+ for ($i=0; $i<5; $i++){
+     var max=0;
+
+     //aceder palavras
+     var words = palavras2.features[$i].properties.palavra;
+     //aceder nº de repetições
+     var repe= palavras2.features[$i].properties.rep;
+     //aceder coordenadas
+     var coord = palavras2.features[$i].geometry.coordinates[$i];
+
+     max = (max < parseFloat(repe)) ? parseFloat(repe) : max;
+
+     var composicao_cont = document.getElementById("divisao");
+
+     //var size = palavras2[prop] * 80 + "%";
+     var opacity = repe / max;
+
+     /*Criar tag p para inserir as palavras*/
+     var palavra_cont = document.createElement("div");
+     palavra_cont.className = "palavra_cont";
+
+     composicao_cont.appendChild(palavra_cont);
+
+     var palavra = document.createElement("p");
+
+     palavra_cont.appendChild(palavra);
+
+     //palavra.innerHTML = words;
+
+     composicao_cont.style.display = "flex";
+
+     /*Estilizar as palavras*/
+     //palavra.style.fontSize = size;
+     palavra.style.fontSize = '50px';
+     palavra.style.textTransform = "uppercase";
+     palavra.style.fontWeight = "bold";
+     palavra.style.fontFamily = "Roboto Condensed, sans-serif";
+     palavra.style.margin = 0;
+     palavra.style.padding = 0;
+     palavra.style.float = "left";
+     palavra.style.opacity= opacity;
+
+//-----------------------------------------------------------------------
+//------------------------------- MAPA -----------------------------------
+//------------------------------------------------------------------------
+     //definir visualização mapa
+     var mymap = L.map('mapid').setView([38.548920, -9.077550], 13);
+
+    // deifnir tile do mapa
+     L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=WdcTGuG5ljgDFUUE3uo3',{
+         attribution: '<a href="https://www.maptiler.com/copyright/" ' +
+             'target="_blank">&copy; ' +
+             'MapTiler</a> <a href="https://www.openstreetmap.org/copyright" ' +
+             'target="_blank">&copy; ' +
+             'OpenStreetMap contributors</a>',
+     }).addTo(mymap);
+
+    //ICON ÁRVORE
+     var arvoreIcon = L.icon({
+         iconUrl: 'img/arvores.png',
+         iconSize: [50, 32]
+     });
+     var arvoreMarker = L.marker([40.2018,-8.4256], {icon: arvoreIcon}).addTo(mymap);
+
+     //COORDENADAS MOUSEOVER
+     L.geoJson(palavras2).addTo(mymap);
+
+     //mouseover
+     var geojson;
+// ... our listeners
+     geojson = L.geoJson(palavras2);
+
+     function highlightFeature(e) {
+         var layer = e.target;
+
+         info.update(layer.feature.properties);
+     }
+
+     function resetHighlight(e) {
+         geojson.resetStyle(e.target);
+         info.update();
+     }
+
+     function zoomToFeature(e) {
+         map.fitBounds(e.target.getBounds());
+     }
+
+     function onEachFeature(feature, layer) {
+         layer.on({
+             mouseover: highlightFeature,
+             mouseout: resetHighlight,
+             click: zoomToFeature
+         });
+     }
+
+     geojson = L.geoJson(palavras2, {
+         onEachFeature: onEachFeature
+     }).addTo(mymap);
+
+     var info = L.control();
+
+     info.onAdd = function (map) {
+
+
+     };
+
+// method that we will use to update the control based on feature properties passed
+     info.update = function (props) {
+         /*this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+             '<b>' + props.palavra
+             : 'Hover over a state');*/
+
+
+         palavra.innerHTML = props.palavra;
+
+     };
+     info.addTo(mymap);
+ }
+
+   /* for (var prop in palavras2) {
 
         max = (max < parseFloat(palavras2[prop])) ? parseFloat(palavras2[prop]) : max;
 
@@ -9,12 +132,12 @@ function compPalavras(palavras2) {
 
         var composicao_cont = document.getElementById("divisao");
 
-        /*Aumentar palavra consoante as vezes que é inserida*/
+        //Aumentar palavra consoante as vezes que é inserida
 
         //var size = palavras2[prop] * 80 + "%";
         var opacity = palavras2[prop] / max;
 
-        /*Crear tag p para inserir as palavras*/
+        //Crear tag p para inserir as palavras
         var palavra_cont = document.createElement("div");
         palavra_cont.className = "palavra_cont";
 
@@ -28,7 +151,7 @@ function compPalavras(palavras2) {
 
         composicao_cont.style.display = "flex";
 
-        /*Estilizar as palavras*/
+        //Estilizar as palavras
         //palavra.style.fontSize = size;
         palavra.style.fontSize = '50px';
         palavra.style.textTransform = "uppercase";
@@ -39,6 +162,5 @@ function compPalavras(palavras2) {
         palavra.style.float = "left";
         palavra.style.opacity=opacity;
 
-    }
-
+    }*/
 }
--- mapa.js
+++ mapa.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([40.2056400, -8.4195500], 13);
+var mymap = L.map('mapid').setView([38.548920, -9.077550], 13);
 
 L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=WdcTGuG5ljgDFUUE3uo3',{
     attribution: '<a href="https://www.maptiler.com/copyright/" ' +
@@ -14,3 +14,59 @@ var arvoreIcon = L.icon({
 });
 
 var arvoreMarker = L.marker([40.2018,-8.4256], {icon: arvoreIcon}).addTo(mymap);
+
+/////////////////////////////////////////
+/*var data= JSON.parse(test);
+
+L.geoJson("test.json").addTo(mymap);
+
+//mouseover
+var geojson;
+// ... our listeners
+geojson = L.geoJson(statesData);
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(data, {
+    onEachFeature: onEachFeature
+}).addTo(mymap);
+
+var info = L.control();
+
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>US Population Density</h4>' +  (props ?
+        '<b>' + props.palavra + '</b><br />' + props.rep
+        : 'Hover over a state');
+};
+
+info.addTo(mymap);*/
+
+
