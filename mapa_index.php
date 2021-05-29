32f0a2c1fae5a1aee6263d1168014ed74735d6f1
franciscalaureano <mariakikalaureano1@gmail.com>
2021-05-29 16:13:37 +0100

    mapa_index update

--- mapa_index.php
+++ mapa_index.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Balada Tipográfica</title>
+
     <link rel="stylesheet" href="css/geral.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/saxxtext.min.css" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <link rel="stylesheet" href="css/style.css">
+
 
     <script src="saxxtext.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 
 </head>
